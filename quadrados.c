9bc2f4fdc04ef9562f1f301cd04a04f3b1582681
Valen <valenpschool@gmail.com>
2023-03-24 19:33:01 -0300

    feat: add "quadrados"

---
+++ quadrados.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    printf("O quadrado de 13 é %g\n", pow(13, 2));
+    printf("O quadrado de 15 é %g\n", pow(15, 2));
+    printf("O quadrado de 17 é %g\n", pow(17, 2));
+}
