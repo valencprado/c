c7e4c636227f49e5a0fb34add099691d3c2eb93f
Valen <valenpschool@gmail.com>
2023-03-28 11:40:23 -0300

    feat: add programa de kelvin

---
+++ kelvin.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+// programa que converte Celsius para Farenheit
+
+int main() {
+    printf("Em Kelvin, 0℃ é %d K\n", 0 + 273);
+    printf("Em Kelvin, 36,5℃ é %g K\n", 36.5 + 273);
+    printf("Em Kelvin, 100℃ é %d K\n", 100 + 273);
+}
