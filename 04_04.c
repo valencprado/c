7d6214057d72d51124df4fc21e79b53e272028e2
valen <valenpschool@gmail.com>
2023-04-04 21:31:26 -0300

    feat: finalização da aula

--- 04_04.c
+++ 04_04.c
@@ -1,16 +1,48 @@
 #include <stdio.h>
 // aula do dia 04/04
-
-int main(void) {
 // estruturas condicionais, operadores relacionais e operadores lógicos
-  int idade = 0;
 
-  printf("Digite a idade: ");
-  scanf("%d", &idade);
-  if(idade >= 18) {
-      printf("Pode passar pois voce tem %d anos.", &idade);
-  } else {
-      printf("Nao pode passar pois voce tem %d anos.", &idade);
+int main(void)
+{
+  /*  // verificação de idade para dirigir
+   int idade = 0;
+
+   printf("Digite a idade: ");
+   scanf("%d", &idade);
+   if(idade >= 18) {
+       printf("Pode passar.\n");
+   } else {
+       printf("Nao pode passar.\n");
+   }
+   return 0;
+*/
+  /*
+ verificação se números lidos são iguais
+ int numero1 = 0;
+ int numero2 = 0;
+ printf("Digite o primeiro numero inteiro.");
+ scanf("%d", &numero1);
+ printf("Digite outro numero inteiro.");
+ scanf("%d", &numero2);
+
+ if (numero1 == numero2) {
+   printf("Esses numeros sao iguais");
+ } else {
+   printf("Esses numeros nao sao iguais.");
+ }
+ return 0;
+*/
+  // verifica se o número digitado é negativo ou positivo
+  int numero = 0;
+  printf("Digite um numero inteiro bem legal: ");
+  scanf("%d", &numero);
+  if (numero < 0)
+  {
+    printf("O numero e negativo");
   }
-  return 0;
+  else
+  {
+    printf("O numero e positivo");
+  }
+    return 0;
 }
