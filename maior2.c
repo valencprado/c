ca329449a2153260d9996dba71583ae34c09c61b
Valen <valenpschool@gmail.com>
2023-04-05 17:53:08 -0300

    feat: add programa

--- maior2.c
+++ maior2.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-    
+    int numeroA = 0, numeroB = 0;
+  
+  printf("Digite o numero A: ");
+  scanf("%d", &numeroA);
+  printf("Digite o numero B: ");
+  scanf("%d", &numeroB);
+//   int temp = ;
+if(numeroA == numeroB) {
+    printf("Numeros iguais");
+} else {
+    numeroA > numeroB ? printf("Primeiro maior") : printf("Segundo maior");
+}
 }
