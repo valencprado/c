5a74ca5ee3e2225543e5ca709b5e4f1e203bb12f
Valen <valenpschool@gmail.com>
2023-04-18 22:03:24 -0300

    feat: loop com alunos

---
+++ alunos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+Construa um programa que pergunte a idade e altura de uma turma de alunos, terminando quando for digitada uma idade igual a zero. Use alturas em metros. O programa deve imprimir três frases em linhas distintas:
+a)      "A classe tem %d alunos"
+b)      "A idade media dos alunos com menos de 1,70m de altura e' %g". Se não houver alunos nessa condição, deverá ser impresso "Nao ha' alunos com menos de 1,70m de altura".
+c)       "A altura media dos alunos com mais de 20 anos e' %g". Se não houver alunos nessa condição, deverá ser impresso "Nao ha' alunos com mais de 20 anos".
+
+Dica: guarde as somas e quantidades e deixe para calcular as médias no final. Confira os exemplos.
+*/
+int main(void) {
+  
+}
