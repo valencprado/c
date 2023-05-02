adb33787ab3a8e3a28d35b282f253cd9ec0da4b5
Valen <valenpschool@gmail.com>
2023-05-02 10:42:48 -0300

    refactor: formatação

--- fibonacci.c
+++ fibonacci.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-int main() {
-    int n, contador = 3, num1 = 1, num2 = 1, soma = 0;
+int main()
+{
+	int n, contador = 3, num1 = 1, num2 = 1, soma = 0;
 	printf("Digite N: ");
 	scanf("%d", &n);
+	
 	if (n > 2)
 	{
 		printf("0 1 1 ");
-		while(contador != n)
+		while (contador != n)
 		{
 			soma = num1 + num2;
-			if(num1 >= num2)
+			if (num1 >= num2)
 				num2 = soma;
 			else
 				num1 = soma;
